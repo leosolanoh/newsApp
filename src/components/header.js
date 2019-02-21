be3771b68afd5f8030a58e2e7ec86e36f4fc22d8
leosolanoh <leosolhe@hotmail.com>
2019-02-20 16:42:50 -0800

    second pattern  active true/false

--- src/components/header.js
+++ src/components/header.js
@@ -13,49 +13,26 @@ class Header extends Component {
 
     state = {
         keywords: '',
-    
+        active: false
     }
 
     inputChangeHandler = (e) => {
-      //  const value = e.target.value === '' ? 'non-active' : 'active';
-        this.setState({
-         //   status: value,
+        //detects if empty or not 
+      const val = e.target.value === '' ? false : true;
+       
+        this.setState({  
+            active: val,     
             keywords: e.target.value });        
     }
 
     render() {
-      
-      /*const styles = {
-            header:{
-                background: '#03a9f4' ,
-                padding: '10px'
-            },
-            logo:{
-                color:'#fff',
-                fontFamily:'Source Code Pro',
-                textAlign:'center',
-                fontSize: '25px'
-            }              
-        }
-           //  inside return
-              <header style={styles.header}>
-              <div style={styles.logo}>Logo</div> 
-        -------------------------------------------------*/
-        const style = {
-            backgroundImage: 'red'
-        }
-       
-
-        
-       if (this.state.keywords !== '') 
-            { style.background = 'blue' } 
-       else 
-            { style.background = 'green'; } 
-        
-
+          /* Second pattern
+          using state active true/false to change background  colors when  
+          a character typed */
         return (
-        // first style mode
-        <header style={style}>
+     
+
+        <header  style={{background: `${this.state.active ? 'red' : 'blue'}`}} >
             <div className="logo">Logo</div>
             <input onChange={this.inputChangeHandler} type='text' />
         </header>
