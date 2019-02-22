9455ff853c246762b3d2010ad79155bf5d4609ac
leosolanoh <leosolhe@hotmail.com>
2019-02-21 19:08:33 -0800

    configuring CSS modules   and set styles using css modules

---
+++ build/precache-manifest.03ab706f2a74a0b521e1dc5b6b7231b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8441535bca7b896baa7a",
+    "url": "/static/css/main.4493df3d.chunk.css"
+  },
+  {
+    "revision": "8441535bca7b896baa7a",
+    "url": "/static/js/main.8441535b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "72b6d25545a2f8cbcce3",
+    "url": "/static/js/2.72b6d255.chunk.js"
+  },
+  {
+    "revision": "9f3b8eb7bc4ce2ac43443df70fdebdd8",
+    "url": "/index.html"
+  }
+];
