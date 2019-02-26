c73eeb80b001babce837edc127d156c6286acb0e
leosolanoh <leosolhe@hotmail.com>
2019-02-26 09:01:53 -0800

    filter feeds by title

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 //import './header.css'
 import classes  from '../css/styles.css'; 
 
-class Header extends Component {
-
-    state = {
-        keywords: ''      
-    }
-
-    inputChangeHandler = (e) => {
-     
-        this.setState({  
-          
-            keywords: e.target.value });        
-    }
-
-    render() {
-          /* Fourth pattern
-          using CSS library glamor   */
-        return (
-     
-
-        <header className={classes.logo} >
-            <div>Logo</div>
-            <input onChange={this.inputChangeHandler} type='text' />
-        </header>
-        )
-    } 
+const Header =  (props) => {   
+            
+        return (        
+            <header className={classes.logo} >
+                <div>Logo</div>
+                <input onChange={ props.keywords } type='text' />
+            </header>
+        )     
 
 }
 export default Header; 
+
+
+
+
+
+ 
--- src/index.js
+++ src/index.js
@@ -7,17 +7,38 @@ import Header from './components/header'
 //import Newslist from './components/temporal';
 import Newslist from './components/newsList';
 
+
 class App extends Component {   
 
     state = {
-        news : JSON
+        news : JSON,
+        filtered: []
+    }
+
+    getKeyword = (event) =>{
+       // console.log(event.target.value);
+       let keyword = event.target.value;
+       let filtered = this.state.news.filter((item)=>{
+         return item.title.includes(keyword)
+       })
+       this.setState({
+           filtered:filtered
+        });
     }
 
     render(){
-         
+        const centro={
+            textAlign:'center',
+            display: 'block',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            width: '40%'
+        }
+         let newsFiltered = this.state.filtered;
         return (<div>
-                    <Header />
-                    <Newslist news={this.state.news}>
+                    <Header keywords={ this.getKeyword }/>
+                    
+                    <Newslist news={newsFiltered.length === 0 ? this.state.news : newsFiltered}>
                         <h1>The news are:</h1>                 
                     </Newslist>
                 </div> 
